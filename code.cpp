4d46e05e2b489decfa05cfe2c4ecef762a1f4ecd
Brianmax <george.maxi@ucsp.edu.pe>
2022-02-14 23:28:39 -0500

    libreria nodo agregada

--- code.cpp
+++ code.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include "node.h"
 using namespace std;
 
 int main()
@@ -9,5 +10,6 @@ int main()
     cout<<"Linea 4"<<endl;
     cout<<"Linea 5"<<endl;
     cout<<"Linea 6"<<endl;
+    node<int> nodo1;
     return 0;
 }
